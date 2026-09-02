14cf9dc0573fa90538b1f1ea104588ca5be08aeb
Roman Contreras <roman.contreras@ciencias.unam.mx>
2026-09-02 11:09:59 -0600

    feat: agrega metodo `upsert`

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,25 @@ impl<T> SimpleHashMap<T> {
         Ok(())
     }
 
+    pub fn upsert(&mut self, key: usize, value: T) -> Option<T> {
+        let index = self.hash(key);
+
+        let bucket = self.buckets.get_mut(index).unwrap();
+
+        let n = bucket.iter().enumerate().find(|(_, (i, _))| key == *i);
+
+        match n {
+            Some((n, _)) => {
+                let (_, v) = bucket.remove(n);
+
+                bucket.push((key, value));
+
+                Some(v)
+            }
+            None => None,
+        }
+    }
+
     pub fn buscar(&self, key: usize) -> Option<&T> {
         let index = self.hash(key);
         self.buckets[index]
@@ -126,6 +145,18 @@ mod tests {
         let val = hm.buscar(4);
 
         assert_eq!(val, None)
+    }
+
+    #[test]
+    fn test_upsert() {
+        let mut hm = SimpleHashMap::new();
+
+        let _ = hm.insertar(3, "hola");
+        let res = hm.upsert(3, "mundo");
+        assert_eq!(res, Some("hola"));
+
+        let replaced = hm.buscar(3);
+        assert_eq!(replaced, Some(&"mundo"))
     }
 
     #[test]
